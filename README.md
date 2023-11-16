3e005e66ccf301fec141eda70a8f7f36d9d75fa9
Thomas Eding <thomaseding@gmail.com>
2023-11-16 08:01:04 -0800

    Working demo.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Demo of OpenAI API for GPT with Vision
+
+## Setup
+https://platform.openai.com/docs/guides/vision
+```
+pip install --upgrade openai
+pip install python-dotenv
+```
+
+## Using the "openai" library
+```
+python -m demo.openai
+```
+
+## Using the "requests" library
+```
+python -m demo.requests
+```
---
+++ demo/openai.py
@@ -0,0 +1,84 @@
+import base64
+import dotenv
+import openai
+import os
+import typing
+
+from typing import Literal
+
+
+dotenv.load_dotenv()
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+
+def encode_image(image_path: str) -> str:
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+
+def go_image(
+    *,
+    image_path: str,
+    detail: Literal["low", "high", "auto"],
+) -> None:
+    _, ext = os.path.splitext(image_path)
+    ext = ext.lower()
+    base64_image = encode_image(image_path)
+
+    tag: str
+    if ext == ".jpg" or ext == ".jpeg":
+        tag = "jpeg"
+    elif ext == ".png":
+        tag = "png"
+    else:
+        raise Exception("Unsupported image type")
+
+    model = "gpt-4-vision-preview"
+
+    client = openai.OpenAI(
+        api_key=OPENAI_API_KEY,
+    )
+
+    # The API is typed incorrectly, so we need to cast the literal.
+    ROLE_SYSTEM = typing.cast(Literal["user"], "system")
+    ROLE_USER = "user"
+
+    response = client.chat.completions.create(
+        model=model,
+        messages=[
+            {
+                "role": ROLE_SYSTEM,
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "What's in this image?",
+                    },
+                ],
+            },
+            {
+                "role": ROLE_USER,
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": f"data:image/{tag};base64,{base64_image}",
+                            "detail": detail,
+                        },
+                    },
+                ],
+            },
+        ],
+    )
+
+    print(response)
+
+
+def main():
+    go_image(
+        image_path="data/sample.png",
+        detail="auto",
+    )
+
+
+if __name__ == "__main__":
+    main()
